2401187d3caf235116ea43908f5861a0b063a8c4
Jesus Bejar <israelbejarii@gmail.com>
2025-02-21 12:49:12 -0700

    added comments

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// get elements
+
+// generate idea event
+
+// clear input
+
+// create element to add to list (add delete button funcionality)
+
+// add idea event ( add element to local storage also)
+// localStorage.setItem('Name', 'Idea1')
